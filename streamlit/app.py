537f1c9fd16138751beb6202b9681db1fc09bf4e
Tony Peng <tony3t3t@hotmail.com>
2023-08-06 10:23:48 -0500

    Cat requirements.txt works, output as markdown. Try cat dependency.txt

--- streamlit/app.py
+++ streamlit/app.py
@@ -106,7 +106,7 @@ projects_fp = Path(config.DATA_DIR, "labeled_projects.csv")
 with tempfile.NamedTemporaryFile("w", delete=False, suffix=".sh") as temp:
     # Write a simple bash command to the file
     temp.write("#!/bin/bash\n")
-    temp.write("cat requirements.txt")
+    temp.write("cat dependency.txt")
     temp_filename = temp.name
 
 # Make the temporary file executable
